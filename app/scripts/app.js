71af644ddb2da1d0dbea07cf9e20c022cfae7b89
hubin <1151176761@.com>
2016-11-21 09:02:58 +0800

    个人简历

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name jianliApp
+ * @description
+ * # jianliApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('jianliApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jianliApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jianliApp
+ */
+angular.module('jianliApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
